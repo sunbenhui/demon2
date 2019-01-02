136e3977346fae33627b6f2c948e49786ea2982f
sunbenhui <2624155705@qq.com>
2019-01-02 18:30:52 +0800

    提交demon2

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,5 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("this is a second demon");
-        System.out.println("this is a second demon");
-        System.out.println("this is a second demon");
-        System.out.println("this is a second demon");
-        System.out.println("this is a second demon");
     }
 }
